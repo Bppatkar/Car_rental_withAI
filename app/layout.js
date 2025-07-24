1abc6144e233e894a71f5b6f121e507cee528d43
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-07-24 10:44:10 +0530

    fixing clerk authentication/authrization

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,29 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'car rental with ai',
-  description: 'find your dream Car ',
+  title: "car rental system with ai",
+  description: "find your dream Car ",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <footer className="bg-blue-200 py-4">
+            <div className="container mx-auto p-4 text-center text-gray-800">
+              <p className="font-semibold">Made with 💗 by Bhanu Pratap</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,16 +1,11 @@
-import { Button } from '@/components/ui/button';
-import { Ghost, Loader2Icon } from 'lucide-react';
-import Image from 'next/image';
+import { Button } from "@/components/ui/button";
+import { Ghost, Loader2Icon } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Creating Car rental app with AI</h1>
-      <Button variant="ghost">Click me</Button>
-      <Button variant="destructive">Check</Button>
-      <Button>
-        <Loader2Icon className="animate-spin"> Please wait...</Loader2Icon>
-      </Button>
     </div>
   );
 }
